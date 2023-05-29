3754e08b622ce9d2d0c0aa6156bd4370d1325fa6
kaiking06 <128317144+kaiking06@users.noreply.github.com>
2023-05-29 16:52:31 -0700

    new js lines

---
+++ index.js
@@ -0,0 +1,44 @@
+const a = 2;
+const b = 6;
+const result=(a+b)
+console.log(result);
+
+
+var first = "my name is first ";
+var second = "my name is second";
+console.log(first+second)
+
+
+const c = 4
+const d = 5
+const e = 7
+if(c===4 && d===5 && e===7){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+const g = 1
+const f = 3
+if(f<g){
+    console.log(g+f)
+}else{
+    console.log(g+f/2)
+}
+
+
+const data ={
+    name:'luther'
+    surname:'kai'
+    email:'kaiking@gmail'
+    address:'kumba'
+    age:'40'
+
+}
+const lutherName= data.name
+const lutherSurname=data.surname
+const lutherEmail=data.email
+const lutherAddress=data.address
+const lutherAge=data.age
+console.log(lutherName+lutherSurname+lutherEmail+lutherAddress+lutherAge)
